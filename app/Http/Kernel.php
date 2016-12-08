02f14d6a6e1d6f1534d84502cfe63719ddd9f852
Grant Savage <Grant@MacBook-2.local>
2016-12-08 03:27:51 -0700

    Fixed Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            //\App\Http\MiddleWare\AdminVerified::class
+            \App\Http\Middleware\AdminVerified::class
         ],
 
         'api' => [
@@ -53,6 +53,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'adminVerified' => \App\Http\MiddleWare\AdminVerified::class
+        'adminVerified' => \App\Http\Middleware\AdminVerified::class
     ];
 }
